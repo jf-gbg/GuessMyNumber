3aa1ac9318445297d4af1ce42c021a18a8ac932d
jf-gbg <malmmlam@outlook.com>
2022-07-25 10:49:47 +0200

    implement basic game logic

--- main.cpp
+++ main.cpp
@@ -3,6 +3,33 @@ using std::cout;
 using std::cin;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int guess = 0;
+    int answer = 7;
+    bool continuePlaying = true;
+
+    cout << "I am thinking of a number, try to guess what it is. "
+    << '\n'<< "Enter your guess: " << '\n';
+
+
+    while (continuePlaying)
+    {
+        cin >> guess;
+
+        if (guess > answer)
+        {
+            cout << "Your guess was too high! Please try again." << '\n';
+            cout << "Enter your guess: " << '\n';
+        }
+        else if (guess < answer)
+        {
+            cout << "Your guess was too low! Please try again." << '\n';
+            cout << "Enter your guess: " << '\n';
+        }
+        else if (guess == answer)
+        {
+            cout << "Your guess was correct! Well done!" << '\n';
+            continuePlaying = false;
+        }
+    }
     return 0;
 }
